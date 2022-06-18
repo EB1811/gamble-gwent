6c32625d07b164531107ea47e3df979494345590
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-06-18 12:12:43 +0100

    Added vitest

--- gamble-gwent/vite.config.ts
+++ gamble-gwent/vite.config.ts
@@ -1,11 +1,17 @@
-import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 import postcss from './postcss.config.js'
+import {defineConfig} from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   css: {
     postcss
+  },
+  define: {
+    'import.meta.vitest': 'undefined'
+  },
+  test: {
+    includeSource: ['src/**/*.{js,ts}']
   }
 })
