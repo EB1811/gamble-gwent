e6ab25b5f7d98bb69d84e9d561c53653773cb0bf
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-06-18 12:13:41 +0100

    test test

---
+++ gamble-gwent/src/lib/game/gameStateStore.test.ts
@@ -0,0 +1,11 @@
+import {describe, it, afterEach, expect, vi} from 'vitest'
+
+describe('getFullRandomHand', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it(`test`, () => {
+    expect('2').toEqual('2')
+  })
+})
