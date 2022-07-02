ffd4f437a505bc0a1d1d5f2c4c3ef13653ec02dd
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-07-02 19:23:31 +0100

    Temp data + constants states

--- gamble-gwent/src/lib/constants.ts
+++ gamble-gwent/src/lib/constants.ts
@@ -1,10 +1,10 @@
 export type GROUP =
-  | 'PLAYER1_MELEE'
-  | 'PLAYER1_RANGED'
-  | 'PLAYER1_SIEGE'
-  | 'PLAYER2_MELEE'
-  | 'PLAYER2_RANGED'
-  | 'PLAYER2_SIEGE'
+  | 'PLAYER_MELEE'
+  | 'PLAYER_RANGED'
+  | 'PLAYER_SIEGE'
+  | 'ENEMY_MELEE'
+  | 'ENEMY_RANGED'
+  | 'ENEMY_SIEGE'
 
 export const CARD_CLASS = {
   MELEE: 'MELEE' as 'MELEE',
@@ -23,10 +23,77 @@ export const CARD_TYPE = {
 }
 export type CARD_TYPE = keyof typeof CARD_TYPE
 
-export type ROUND_STATES =
-  | 'INITIAL'
-  | 'PLAYER1_TURN'
-  | 'PLAYER2_TURN'
-  | 'PLAYER1_PASS_PLAYER2_TURN'
-  | 'PLAYER2_PASS_PLAYER1_TURN'
-  | 'ROUND_END'
+export const ROUND_STATES = {
+  INITIAL: 'INITIAL' as 'INITIAL',
+  PLAYER_TURN: 'PLAYER_TURN' as 'PLAYER_TURN',
+  ENEMY_TURN: 'ENEMY_TURN' as 'ENEMY_TURN',
+  PLAYER_PASS_ENEMY_TURN: 'PLAYER_PASS_ENEMY_TURN' as 'PLAYER_PASS_ENEMY_TURN',
+  ENEMY_PASS_PLAYER_TURN: 'ENEMY_PASS_PLAYER_TURN' as 'ENEMY_PASS_PLAYER_TURN',
+  ROUND_END: 'ROUND_END' as 'ROUND_END'
+}
+export type ROUND_STATES = keyof typeof ROUND_STATES
+
+export const ROUND_STATE_ACTION = {
+  turnPlayer: 'turnPlayer' as 'turnPlayer',
+  turnEnemy: 'turnEnemy' as 'turnEnemy',
+  nextTurn: 'nextTurn' as 'nextTurn',
+  passTurn: 'passTurn' as 'passTurn'
+}
+export type ROUND_STATE_ACTION = keyof typeof ROUND_STATE_ACTION
+
+export const ROUND_STATE_MACHINE: readonly {
+  initial: ROUND_STATES
+  next: ROUND_STATES
+  action: ROUND_STATE_ACTION
+}[] = [
+  {
+    initial: 'INITIAL',
+    next: 'PLAYER_TURN',
+    action: 'turnPlayer'
+  },
+  {
+    initial: 'INITIAL',
+    next: 'ENEMY_TURN',
+    action: 'turnEnemy'
+  },
+  {
+    initial: 'PLAYER_TURN',
+    next: 'ENEMY_TURN',
+    action: 'nextTurn'
+  },
+  {
+    initial: 'ENEMY_TURN',
+    next: 'PLAYER_TURN',
+    action: 'nextTurn'
+  },
+  {
+    initial: 'PLAYER_TURN',
+    next: 'PLAYER_PASS_ENEMY_TURN',
+    action: 'passTurn'
+  },
+  {
+    initial: 'ENEMY_TURN',
+    next: 'ENEMY_PASS_PLAYER_TURN',
+    action: 'passTurn'
+  },
+  {
+    initial: 'PLAYER_PASS_ENEMY_TURN',
+    next: 'PLAYER_PASS_ENEMY_TURN',
+    action: 'nextTurn'
+  },
+  {
+    initial: 'PLAYER_PASS_ENEMY_TURN',
+    next: 'ROUND_END',
+    action: 'passTurn'
+  },
+  {
+    initial: 'ENEMY_PASS_PLAYER_TURN',
+    next: 'ENEMY_PASS_PLAYER_TURN',
+    action: 'nextTurn'
+  },
+  {
+    initial: 'ENEMY_PASS_PLAYER_TURN',
+    next: 'ROUND_END',
+    action: 'passTurn'
+  }
+]
