cacd08229ed7a7cd471c3f0e0d02aa4a46ff85fd
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-08-28 16:05:44 +0100

    Added drag handler to board

--- gamble-gwent/src/lib/game/Board/battleBoard/BattleBoard.svelte
+++ gamble-gwent/src/lib/game/Board/battleBoard/BattleBoard.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
   import {gameState} from '../../gameStateStore'
   import BattleGroup from './boardGroup/BoardGroup.svelte'
+  import {battleBoardState} from './battleBoardStore'
+  import type {GameCard} from '../../gameTypes'
+
+  const handleDragOver = (e: DragEvent) => {
+    if ($battleBoardState.placeablePosition.includes('NONE')) e.preventDefault()
+  }
+
+  const handleDragDrop = (e: DragEvent): void => {
+    e.preventDefault()
+
+    const placedCardId: string | undefined = e.dataTransfer?.getData('cardId')
+    const placeCard: GameCard | undefined = $gameState.playerHand.find(
+      card => card.id === placedCardId
+    )
+    if (placeCard?.onPlayedEffect) {
+      gameState.playCardEffect(placeCard, placeCard?.onPlayedEffect)
+      gameState.endTurn()
+    }
+    battleBoardState.setPlaceablePosition([])
+
+    console.log('after playCard state', $gameState)
+  }
 </script>
 
 <!-- TODO: Turn into css grid -->
-<div class="mx-auto flex max-w-4xl border-2" style="width: 720px">
+<div
+  class="mx-auto flex max-w-4xl border-2"
+  style="width: 720px"
+  class:bg-gray-300={$battleBoardState.placeablePosition.includes('NONE')}
+  on:dragover={e => handleDragOver(e)}
+  on:drop={e => handleDragDrop(e)}
+>
   <div class="my-auto flex border-2">
     {#each $gameState.boardLayout.globalModifiers as sideGroup}
       <BattleGroup boardGroup={sideGroup} />
